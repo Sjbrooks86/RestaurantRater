339f41e6d5475dfbddbdb1a55d1b938703aa5ad1
Scott Brooks <59925973+Sjbrooks86@users.noreply.github.com>
2020-03-23 19:56:55 -0400

    Create Functionality Added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -10,10 +10,32 @@ namespace RestaurantRater.Controllers
     public class RestaurantController : Controller
     {
         private RestaurantDbContext _db = new RestaurantDbContext();
+ 
         // GET: Restaurant/Index
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList() );
+        }
+
+        //Get: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST : Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
